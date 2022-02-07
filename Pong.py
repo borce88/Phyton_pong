83d45e16b34c0805f587071a4f0d1b577ef280ad
Borce <liberaid@hotmail.com>
2022-02-07 22:59:08 +0100

    apply

--- Pong.py
+++ Pong.py
@@ -6,7 +6,7 @@ wn.bgcolor("black")
 wn.setup(width=800,height=600)
 wn.tracer(0)
 
-
+#Paddles
 #Paddle One
 paddle_one = turtle.Turtle()
 paddle_one.speed(0)
@@ -18,7 +18,6 @@ paddle_one.goto(-350,0)
 
 #Paddle Two
 paddle_two = turtle.Turtle()
-paddle_two.speed(0)
 paddle_two.shape("square")
 paddle_two.color("white")
 paddle_two.shapesize(stretch_wid=5,stretch_len=1)
@@ -26,14 +25,21 @@ paddle_two.penup()
 paddle_two.goto(+350,0)
 
 #Paddle Ball
-Ball = turtle.Turtle()
-Ball.speed(0)
-Ball.shape("square")
-Ball.color("white")
-Ball.penup()
-Ball.goto(0,0)
-Ball.dx = 0.1
-Ball.dy = 0.1
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.1
+ball.dy = -0.1
+
+# Score
+p1score = 0
+p2score = 0
+scoreTable = turtle.Turtle
+scoreTable.speed(0)
+scoreTable.goto(0,0)
 
 # Function
 def paddle_one_up():
@@ -60,7 +66,6 @@ def paddle_two_down():
     if y > -250:
         paddle_two.sety(y)
 
-
 # Keyboard Input
 wn.listen()
 wn.onkeypress(paddle_one_up, "w")
@@ -71,8 +76,33 @@ wn.onkeypress(paddle_two_down,"2")
 # MAIN
 while True:
     wn.update()
-    bxm = Ball.setx(Ball.xcor()-Ball.dx)
-    bym = Ball.sety(Ball.ycor()-Ball.dy)
+    ball.setx(ball.xcor()-ball.dx)
+    ball.sety(ball.ycor()-ball.dy)
+    
     #Border Check
-    
-    
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        
+    #Hit and miss
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_two.ycor() +40 and ball.ycor() > paddle_two.ycor() -40):
+        ball.setx(340)
+        p2score += 1
+        ball.dx *= -1
+                
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_one.ycor() +40 and ball.ycor() > paddle_one.ycor() -40):
+        ball.setx(-340)  
+        p1score +=1
+        ball.dx *= -1
